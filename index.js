4e0b0ad86dac817135ea5302962ba127c7a8f638
thanaha <thanaha@thanahas-MacBook-Pro.local>
2021-04-28 23:48:03 +0700

    app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const http=require('http')
-const app=require('./App')
+const app=require('./app')
 const port=process.env.PORT
 const server=http.createServer(app)
 server.listen(port,(err)=>{
